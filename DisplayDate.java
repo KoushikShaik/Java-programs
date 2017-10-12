6a73a35fffdab8b27a8d42e0826e0d8bf0d359fa
koushii <shaikkoushik56@gmail.com>
2017-10-12 20:03:12 +0530

    Create DisplayDate.java

---
+++ DisplayDate.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DisplayDate {
+   public static void main(String args[]) {
+       // Instantiate an object
+       Date date = new Date();
+        
+       // display time and date
+       System.out.println(date.toString());
+   }
+}
